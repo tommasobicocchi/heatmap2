8c99b60981258983b02acdec85f4e8fc0c9baf95
Roberta Gurrieri <robertagurrieri1992@gmail.com>
2019-12-11 20:38:06 +0100

    ok

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+Rails.application.config.assets.precompile +=
+  %w(*.png *.jpg *.jpeg *.gif vendor/somefile.js vendor/somefile.css \
+     vendor/bootstrap/*.js vendor/bootstrap/*.css \
+     vendor/bootstrap/**/*.js vendor/bootstrap/**/*.css)
