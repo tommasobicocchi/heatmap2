cfb6fdc628cddecec12b98c3b04da983e83654a5
Leonardo Dalla Via <dallavia.leonardo@gmail.com>
2019-12-11 17:52:17 +0100

    chart example changed

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'chart', to: 'pages#chart'
   root to: 'pages#home'
-  resources :documents
+  resources :projects, only: [:create, :index, :show, :new, :destroy]
+  resources :documents do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :projects, only: [:create, :index, :show, :new, :destroy] do
-    resources :charts, only: [:create, :index, :new, :destroy]
+    resources :charts, only: [:create, :index, :new]
   end
+  resources :charts, only: [ :show, :destroy ]
 end
