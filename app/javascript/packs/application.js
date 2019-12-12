2e9630884eed8c3a6bb1bcef0de50a8b439e2c5d
tommasobicocchi <tommaso.bicocchi@gmail.com>
2019-12-12 09:19:28 +0100

    ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,11 @@
 import "bootstrap";
 import "chart.js";
-
-
-
-const loadCsv = () => {
-
-console.log('DOM completamente caricato e analizzato');
-
-}
-
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-     loadCsv();
-});
-
-
 import {barChart, lineChart, doughnutChart} from "../plugin/init_chart"
+
+
+
+
+
 
 barChart();
 lineChart();
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,18 @@
 class Document < ApplicationRecord
   mount_uploader :link, CsvUploader
-    has_many :charts, dependent: :destroy
+  belongs_to :project
+  has_many :charts, dependent: :destroy
+
+
+  def sum_hash
+    container_number = 0
+    sum = Hash.new(0)
+    array.each_with_object(sum) do |hash, sum|
+      container_number = hash.each { |key, value| sum[key] += value.to_i }.map { |k,v| v}
+    end
+    container_number.drop(1)
+    raise
+  end
 
     def to_csv
     array = []
