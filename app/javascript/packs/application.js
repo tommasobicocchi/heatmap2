7b637e8bd176da008dd898871ce4c8d266a6f46f
Liviu Cauia <liviu.cauia@gmail.com>
2019-12-12 15:51:46 +0100

    ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,14 @@ import {barChart, lineChart, doughnutChart} from "../plugin/init_chart"
 
 
 
-
+document.querySelectorAll('td').on('click', function() {
+    var $currentTable = $(this).closest('display-table');
+    var index = $(this).index();
+    $currentTable.find('td').removeClass('red');
+    $currentTable.find('tr').each(function() {
+        $(this).find('td').eq(index).addClass('red');
+    });
+});
 
 
 barChart();
