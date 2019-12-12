4bccca0277369cf090ef61a13ef38615323a3571
Leonardo Dalla Via <dallavia.leonardo@gmail.com>
2019-12-12 14:07:53 +0100

    javascript for charts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,10 @@
 import "bootstrap";
-import "chart.js";
-import {barChart, lineChart, doughnutChart} from "../plugin/init_chart"
+// import {barChart, lineChart, doughnutChart} from "../plugin/init_chart"
+import '../vendor/stimulus';
 
 const headers = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
 const data = [12, 19, 3, 5, 2, 3]
 
-barChart(headers, data);
-lineChart();
-doughnutChart();
+// barChart(headers, data);
+// lineChart();
+// doughnutChart();
