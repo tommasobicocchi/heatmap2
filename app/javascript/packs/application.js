49e82df1bce707e7dd6e38f857a8a56ab826f37b
tommasobicocchi <tommaso.bicocchi@gmail.com>
2019-12-12 15:44:57 +0100

    ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,29 @@
 import "bootstrap";
-import "chart.js";
-import {barChart, lineChart, doughnutChart} from "../plugin/init_chart"
 
-const headers = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
-const data = [12, 19, 3, 5, 2, 3]
 
-barChart(headers, data);
-lineChart();
-doughnutChart();
+const selectcellindex = () => {
+const selectfile = document.querySelectorAll("td");
+console.log(selectfile);
+ selectfile.forEach(element => element.addEventListener("click", (event) => {
+    ennphatize(event.currentTarget.className);
+    //ennphatize(selectfile[i].cellIndex);
+  }));
+};
+
+
+const ennphatize = (x) => {
+ const selectfile = document.querySelectorAll(`.${x}`);
+ selectfile.forEach(element => element.classList.add('red'));
+};
+
+selectcellindex();
+
+
+//for (let i = 0; i < selectfile.length; i++) {
+//  selectfile[i].addEventListener("click", (event) => {
+//    selectfile[i].innerHTML = "";
+//    console.log(selectfile[i]);
+//  })
+//};
+
+
--- app/models/document.rb
+++ app/models/document.rb
@@ -7,14 +7,17 @@ class Document < ApplicationRecord
   def sum_hash
     container_number = 0
     sum = Hash.new(0)
-    array.each_with_object(sum) do |hash, sum|
+    to_data.each_with_object(sum) do |hash, sum|
       container_number = hash.each { |key, value| sum[key] += value.to_i }.map { |k,v| v}
     end
     container_number.drop(1)
-    raise
   end
 
-    def to_csv
+  def get_headers
+    @arrayofhash.first.keys
+  end
+
+    def to_data
     array = []
     csv_file = open(link.url).read
     csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
