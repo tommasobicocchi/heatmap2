5151d2706d05f2efe79a17943e067844d473e51b
Leonardo Dalla Via <dallavia.leonardo@gmail.com>
2019-12-12 09:24:07 +0100

    changes with Rui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,9 @@
 import "bootstrap";
 import {barChart, lineChart, doughnutChart} from "../plugin/init_chart"
 
-barChart();
+const headers = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
+const data = [12, 19, 3, 5, 2, 3]
+
+barChart(headers, data);
 lineChart();
 doughnutChart();
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'chart', to: 'pages#chart'
   root to: 'pages#home'
-  resources :projects, only: [:create, :index, :show, :new, :destroy]
+  resources :projects, only: [:create, :index, :show, :new, :destroy, :edit, :update]
   resources :documents do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :charts, only: [:create, :index, :new]
