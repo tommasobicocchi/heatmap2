0d66c311efc8849fdb1877e3d8bfa236e1a7c1cf
Roberta Gurrieri <robertagurrieri1992@gmail.com>
2019-12-13 09:38:10 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,8 @@ Rails.application.routes.draw do
   get 'charts/preview', to: 'charts#preview'
   root to: 'pages#home'
 
-<<<<<<< HEAD
-  resources :projects, only: [:create, :index, :show, :new, :destroy, :edit, :update]
-=======
+
   resources :projects, only: [:create, :index, :show, :new, :destroy, :edit, :update] do
->>>>>>> afab4c72cdfd45db4a8b70cafcd0913386cd33cb
   resources :documents do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :charts, only: [:create, :index, :new, :update]
