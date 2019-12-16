777cf3a4d630af3f5afed436589fe3b895555ce1
Liviu Cauia <liviu.cauia@gmail.com>
2019-12-16 20:13:20 +0100

    chart index

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
-  has_many :documents
-  has_many :charts, through: :documents
-=======
   has_many :documents, dependent: :destroy
->>>>>>> master
+  has_many :charts, through: :documents
 end
