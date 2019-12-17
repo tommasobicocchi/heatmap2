b8972d9c86fb2f3ff32f3250cb7d525c3cf2aed4
Liviu Cauia <liviu.cauia@gmail.com>
2019-12-17 12:31:01 +0100

    ok

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :documents, dependent: :destroy
   has_many :charts, through: :documents
+  validates :name, presence: true
+  validates :description, presence: true
 end
