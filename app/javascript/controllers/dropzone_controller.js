ad40f2734be7a7b9abfaa9ad160324b79f31a320
tommasobicocchi <tommaso.bicocchi@gmail.com>
2019-12-17 20:24:58 +0100

    ok

--- app/javascript/controllers/dropzone_controller.js
+++ app/javascript/controllers/dropzone_controller.js
@@ -1,22 +1,20 @@
-// //import { Controller } from "stimulus"
+// import { Controller } from "stimulus";
 
-// //export default class extends Controller {
-//   //static targets = [ "input" ]
+// export default class extends Controller {
+//   static targets = [ "input" ]
 
 //   connect() {
 //     console.log('ciao');
-//   }
+//      }
 
-//   uploader(event) {
-//     const thing = this.scope.element
-//   }
+//     uploader(event) {
+//        const thing = this.scope.element
+//         }
 
-
-//   createDropzone () {
-//   return new Dropzone(thing);
-// }
-// }
-
+//         createDropzone () {
+//           return new Dropzone(thing);
+//         }
+//       }
 
 // createDropzone(controller);
 
